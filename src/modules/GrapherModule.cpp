5c6edf8ccd1eb864d99bfe8734062a1bcfcdd953
matrefeytontias <mattias@refeyton.fr>
2018-10-03 14:00:46 +0200

    Fixed crash when resizing too small

--- src/modules/GrapherModule.cpp
+++ src/modules/GrapherModule.cpp
@@ -135,7 +135,7 @@ void GrapherModule::plotFunction(int w, int h)
     drawList->AddLine(ImVec2(top.x, origin.y), ImVec2(top.x + w, origin.y), col32, 1);
     
     // Draw the X axis' ticks
-    int nbTicks = (w / xTickSpace) & ~1;
+    int nbTicks = std::max(1, (w / xTickSpace) & ~1);
     for(int k = 0; k <= nbTicks; k++)
     {
         int x = top.x + w * k / nbTicks;
@@ -153,7 +153,7 @@ void GrapherModule::plotFunction(int w, int h)
     // Draw the Y axis' ticks
     if(yrange > 0)
     {
-        nbTicks = (h / yTickSpace) & ~1;
+        nbTicks = std::max(1, (h / yTickSpace) & ~1);
         for(int k = 0; k <= nbTicks; k++)
         {
             int y = bot.y - h * k / nbTicks;
